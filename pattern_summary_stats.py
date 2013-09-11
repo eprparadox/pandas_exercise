63ddb42355129186dd81873476a7dd32d39efa19
jvytlacil <jason.vytlacil@gmail.com>
2013-09-10 17:03:18 -0700

    added several plot updates

--- pattern_summary_stats.py
+++ pattern_summary_stats.py
@@ -11,16 +11,30 @@ import pandas as pd
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import os
+
+## make sure we have space to save
+directory, dummy = os.path.split(os.path.realpath(file_IN))
+if not os.path.exists(directory + '/figures/'):
+	os.mkdir(directory + '/figures/')
+	os.mkdir(directory + '/figures/accuracy')
+	os.mkdir(directory + '/figures/BOLD')
+	os.mkdir(directory + '/figures/distintiveness')
+	os.chdir(directory + '/figures/')
+
 
 ### read in data
 data = pd.read_table(file_IN, sep='\t')
 
 ### parse name and report
-#metadata = file_IN.rsplit('_')
-#the_pen = metadata[-1]; last = the_pen.split('.')[0]; last = test.split('.')[0]
+metadata = file_IN.rsplit('_')
+the_pen = metadata[-1]; last = the_pen.split('.')[0]; the_pen = last[0]
+dataset = metadata[0]; scheme = metadata[2];
+classifier_method = metadata[5]; filter_type = metadata[6]
 
-#	print '-{0}- data set using -{1}- and the -{2}- classifier, -{3}- filtering \
-#		and penalty -{4}-.'.format(metadata[0],metadata[2],metadata[5],metadata[6],last)
+print '-{0}- data set using -{1}- and the -{2}- classifier, -{3}- filtering \
+	and penalty -{4}-.'.format(dataset, scheme,classifier_method,filter_type,last)
+	#metadata[0],metadata[2],metadata[5],metadata[6],last)
 
 ### remove column of NaNs
 del data['Unnamed: 9']
@@ -37,11 +51,45 @@ metrics = list(metrics)
 #stage1 = data[data['measure'] == 'total_BOLD']
 
 for metric in metrics:
+	
+	#############################
+	## first do the line plots ##
+	#############################
+
 	stage1 = data[data['measure'] == metric]	
 	stage1.set_index('TR')
 	table = pd.pivot_table(stage1,values='data',rows=['TR'],cols=['mask'])
 	
+	## lower the lengend case 
+	table.rename(columns=lambda x: x.lower(), inplace=True)
 	## plot this metric over the entire trial
-	table.plot()
-	time.sleep(5.5)    # pause 5.5 seconds
-	plt.close("all")
+	ax = table.plot()
+	# later
+	#ax.set_xticklabels(str(np.arange(1,25,2)),fontsize=18)
+
+	## fancy up the plot
+	plt.legend(frameon=False)
+	plt.grid('off')
+
+	## fonts
+	plt.xlabel(ax.get_xlabel(),fontsize=20)
+	plt.ylabel(ax.get_ylabel(),fontsize=20)
+
+	# limits
+	plt.xlim(ax.get_xlim()[0]+1,ax.get_xlim()[1]-1)
+
+
+	if metric == 'total_accuracy':
+		plt.ylim(0.30,plt.ylim()[1])
+
+	## clean up metric name	
+	plt.ylabel(metric.replace('_',' '))
+
+	plt.show()
+	time.sleep(3)
+	
+	## save
+
+	plt.close('all')
+
+	
